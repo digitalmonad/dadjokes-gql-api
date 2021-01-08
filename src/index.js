3b22cd9b9d6721430ab60fced3d895bd000c9f7a
Pavel Kocman <kocmanpavel@gmail.com>
2021-01-08 12:59:56 +0100

    add server

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { Joke, sequelize } = require("../models");
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Joke {
+    text: String
+    id: ID!
+  }
+
+  type Query {
+    joke: Joke
+  }
+`;
+
+const resolvers = {
+  Query: {
+    joke: async (_, args) => {
+      const joke = await Joke.findOne({
+        order: sequelize.random(),
+      });
+
+      return joke;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: PORT }, () =>
+  console.log(`Server running on port ${PORT}`)
+);
